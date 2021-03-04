923585c49a100c4d5f73588a07fb69907fb41843
kingurru <kingnikoriginal@gmail.com>
2021-03-04 17:30:07 +0300

    init: start TAP task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return array === undefined || array.length === 0 ? 0 : Math.min.apply(null, array)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return array === undefined || array.length === 0 ? 0 : Math.max.apply(null, array)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0
+    } else return array.reduce((a, b) => a + b) / array.length
 }
